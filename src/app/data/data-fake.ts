4ae70e56c2387db212f601ff2a54aff3659768ea
Dessa97 <116980638+Dessa97@users.noreply.github.com>
2022-12-26 10:48:59 -0300

    content created

--- src/app/data/data-fake.ts
+++ src/app/data/data-fake.ts
@@ -1,8 +1,8 @@
 export const dataFake = [
   {
     id: '1',
-    title: 'saiu novo filme',
-    description: 'ola ola',
+    title: 'TÍTULO ARTIGO 1',
+    description: 'is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.',
     photoCover:
       'https://www.unfe.org/wp-content/uploads/2019/04/SM-placeholder.png',
   },
